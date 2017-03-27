634920e97b22cfb05854896dddefe338c31e261f
chasdream <zhb199107@163.com>
2017-03-27 11:47:14 +0800

    aes

---
+++ src/main/java/com/moho/commons/des/AESUtils.java
@@ -0,0 +1,110 @@
+/* 
+ * Copyright (C), 2017-2018
+ * File Name: @(#)haibo
+ * Encoding UTF-8
+ * Author: haibo
+ * Version: 1.0
+ * Date: 2017-03-27
+ */
+package com.moho.commons.des;
+
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+/**
+ * 加密解密
+ * <p>
+ * <p>
+ * <a href="haibo"><i>View Source</i></a>
+ *
+ * @author haibo
+ * @version 1.0
+ * @since 1.0
+ */
+public final class AESUtils {
+
+    private AESUtils() {
+
+    }
+
+    public final static String AES = "AES";
+
+    /**
+     * 创建并初始化密码器
+     *
+     * @param key   加解密密钥
+     * @param model 加解密类型
+     * @return
+     * @throws NoSuchAlgorithmException
+     * @throws NoSuchPaddingException
+     * @throws InvalidKeyException
+     */
+    private static Cipher cipher(byte[] key, int model) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+        KeyGenerator gen = KeyGenerator.getInstance(AES);
+        gen.init(128, new SecureRandom(key));
+        SecretKey secretKey = gen.generateKey();
+        byte[] enCodeFormat = secretKey.getEncoded();
+        SecretKeySpec spec = new SecretKeySpec(enCodeFormat, AES);
+        // 创建密码器
+        Cipher cipher = Cipher.getInstance(AES);
+        // 初始化
+        cipher.init(model, spec);
+        return cipher;
+    }
+
+    /**
+     * aes加密算法
+     *
+     * @param data 待加密的数据
+     * @param key  加密密钥
+     * @return
+     */
+    public static byte[] encrypt(byte[] data, byte[] key) {
+        try {
+            Cipher cipher = cipher(key, Cipher.ENCRYPT_MODE);
+            byte[] byteData = data;
+            byte[] result = cipher.doFinal(byteData);
+            return result;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * aes解密算法
+     *
+     * @param data 待解密数据
+     * @param key  解密密钥
+     * @return
+     */
+    public static byte[] decrypt(byte[] data, byte[] key) {
+        try {
+            Cipher cipher = cipher(key, Cipher.DECRYPT_MODE);
+            byte[] result = cipher.doFinal(data);
+            return result;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/main/java/com/moho/commons/des/MD5Utils.java
+++ src/main/java/com/moho/commons/des/MD5Utils.java
@@ -60,7 +60,24 @@ public final class MD5Utils {
      */
     public static String md5(String param) {
         String md5 = new String();
-
+        try {
+            MessageDigest mDigest = MessageDigest.getInstance("MD5");
+            mDigest.update(param.getBytes());
+            byte b[] = mDigest.digest();
+            int i;
+            StringBuffer buf = new StringBuffer();
+            for (int offset = 0; offset < b.length; offset++) {
+                i = b[offset];
+                if (i < 0)
+                    i += 256;
+                if (i < 16)
+                    buf.append("0");
+                buf.append(Integer.toHexString(i));
+            }
+            md5 = buf.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
         return md5;
     }
 
